9de41f526c1f41fb6e99aae9b1879de848ab335d
Hyeongyu Lee <dlgusrb1913@naver.com>
2021-11-30 22:22:36 +0900

    Create Baekjoon_1011_centauri.java
    백준 센타우리

---
+++ algorithm/src/basic/Baekjoon_1011_centauri.java
@@ -0,0 +1,32 @@
+package basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_1011_centauri {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i < T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			int distance = y - x;
+			int max = (int)Math.sqrt(distance);
+			
+			if(max == Math.sqrt(distance)) {
+				sb.append(2 * max - 1).append("\n");
+			}else if(distance <= max * max + max) {
+				sb.append(2 * max).append("\n");
+			}else {
+				sb.append(2 * max + 1).append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+}
