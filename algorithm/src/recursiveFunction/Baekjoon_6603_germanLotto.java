9710b835da529b85903b0b2e465ff71a255720bf
Hyeongyu Lee <dlgusrb1913@naver.com>
2021-12-16 22:58:59 +0900

    Update Baekjoon_6603_germanLotto.java
    need_to_update

--- algorithm/src/recursiveFunction/Baekjoon_6603_germanLotto.java
+++ algorithm/src/recursiveFunction/Baekjoon_6603_germanLotto.java
@@ -6,12 +6,47 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Baekjoon_6603_germanLotto {
+	public static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
 		while(true) {
-			
+			st = new StringTokenizer(br.readLine());
+			int k = Integer.parseInt(st.nextToken());
+			if(k == 0) {
+				break;
+			}
+			int[] numbers = new int[k];
+			for(int i = 0; i < k; i++) {
+				numbers[i] = Integer.parseInt(st.nextToken());
+			}
+			pick(numbers, k);
+			sb.append('\n');
+		}
+		System.out.println(sb);
+	}
+	
+	public static void pick(int[] numbers, int length) {
+		int[] picks = new int[length - 1];
+		if(length == 6) {
+			for(int i = 0; i < 6; i++) {
+				sb.append(numbers[i]).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		for(int i = 0; i < length; i++) {
+			int index = 0;
+			for(int j = 0; j < length - 1; j++) {
+				if(length - 1 - index == i) {
+					index++;
+				}
+				picks[j] = numbers[index];
+				index++;
+			}
+			pick(picks, length - 1);
 		}
 	}
 }
