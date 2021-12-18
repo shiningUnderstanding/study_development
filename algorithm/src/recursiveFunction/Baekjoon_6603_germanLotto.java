45ce18afec874ee557a8e09d7717dd90e06657f8
Hyeongyu Lee <dlgusrb1913@naver.com>
2021-12-18 23:29:18 +0900

    Update Baekjoon_6603_germanLotto.java

--- algorithm/src/recursiveFunction/Baekjoon_6603_germanLotto.java
+++ algorithm/src/recursiveFunction/Baekjoon_6603_germanLotto.java
@@ -6,6 +6,9 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Baekjoon_6603_germanLotto {
+	static int k;
+	static int[] numbers;
+	static boolean[] picks;
 	public static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws IOException{
@@ -13,40 +16,36 @@ public class Baekjoon_6603_germanLotto {
 		StringTokenizer st;
 		while(true) {
 			st = new StringTokenizer(br.readLine());
-			int k = Integer.parseInt(st.nextToken());
+			k = Integer.parseInt(st.nextToken());
 			if(k == 0) {
 				break;
 			}
-			int[] numbers = new int[k];
+			numbers = new int[k];
+			picks = new boolean[k];
 			for(int i = 0; i < k; i++) {
 				numbers[i] = Integer.parseInt(st.nextToken());
 			}
-			pick(numbers, k);
+			pick(0, 0);
 			sb.append('\n');
 		}
 		System.out.println(sb);
 	}
 	
-	public static void pick(int[] numbers, int length) {
-		int[] picks = new int[length - 1];
-		if(length == 6) {
-			for(int i = 0; i < 6; i++) {
-				sb.append(numbers[i]).append(' ');
+	public static void pick(int start, int depth) {
+		if(depth == 6) {
+			for(int i = 0; i < k; i++) {
+				if(picks[i]) {
+					sb.append(numbers[i]).append(' ');
+				}
 			}
 			sb.append('\n');
 			return;
 		}
 		
-		for(int i = 0; i < length; i++) {
-			int index = 0;
-			for(int j = 0; j < length - 1; j++) {
-				if(length - 1 - index == i) {
-					index++;
-				}
-				picks[j] = numbers[index];
-				index++;
-			}
-			pick(picks, length - 1);
+		for(int i = start; i < k; i++) {
+			picks[i] =true;
+			pick(i + 1, depth + 1);
+			picks[i] = false;
 		}
 	}
 }
