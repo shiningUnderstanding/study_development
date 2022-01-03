259d340e37d796da59684a4155430f0d6238d22c
Hyeongyu Lee <dlgusrb1913@naver.com>
2022-01-03 19:07:11 +0900

    Update Leet_2_AddTwoNumber.java

--- data_structure/src/linked_list/Leet_2_AddTwoNumber.java
+++ data_structure/src/linked_list/Leet_2_AddTwoNumber.java
@@ -1,26 +1,38 @@
 package linked_list;
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 public class Leet_2_AddTwoNumber {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode node = new ListNode(0);
         ListNode result = node;
         int sum = 0;
         
-        while(l1 != null || l2 == null || sum > 0){
-            if(l1 != null){
+        while (l1 != null || l2 != null || sum > 0) {
+            if (l1 != null) {
                 sum += l1.val;
                 l1 = l1.next;
             }
             
-            if(l2 != null){
+            if (l2 != null) {
                 sum += l2.val;
                 l2 = l2.next;
             }
             
             node.next = new ListNode(sum % 10);
             sum /= 10;
+            
             node = node.next;
         }
+        
         return result.next;
     }
 }
