ca272d29812f6b1e45238ca9c83b7716937f0214
Hyeongyu Lee <dlgusrb1913@naver.com>
2022-01-12 23:29:47 +0900

    Create PatternClass.java

---
+++ algorithm/src/RegularExpression/PatternClass.java
@@ -0,0 +1,70 @@
+package RegularExpression;
+
+import java.util.regex.Pattern;
+
+public class PatternClass {
+	//정규식 비교함수
+	static void regExp(String regExp, String data) {
+		boolean result = Pattern.matches(regExp, data);
+		if(result) {
+			System.out.println("정규식: " +regExp +", 문자열: " + data + ", 정규식과 일치합니다.");
+		}else{
+			System.out.println("정규식: " +regExp +", 문자열: " + data + ", 정규식과 일치하지 않습니다.");
+		}
+	}
+	
+	public static void main(String[] args) {
+		String regExp = "[a]";
+		String data = "a";
+		regExp(regExp, data);
+		
+		//010-xxxx-xxxx
+		regExp = "(010)-\\d{4}-\\d{4}";
+		data = "011-6232-9789";
+		regExp(regExp, data);
+		
+		regExp = "010-2154-1542";
+		data = "010-2154-1542";
+		regExp(regExp, data);
+		
+		//-----------------------------
+		// abc
+		regExp = "abc";
+		data = "abc";
+		regExp(regExp, data);
+		
+		regExp = "abc";
+		data = "abc";
+		regExp(regExp, data);
+		
+		// 123
+		regExp = "123";
+		data = "123";
+		regExp(regExp, data);
+		
+		// \.
+		regExp = "\\.";
+		data = ".";
+		regExp(regExp, data);
+		
+		// []
+		regExp = "^[abc][a-z]$";
+		data = "ac";
+		regExp(regExp, data);
+		data = "bs";
+		regExp(regExp, data);
+		data = "ab";
+		regExp(regExp, data);
+		data = "a^";
+		regExp(regExp, data);
+		
+		// [^]
+		regExp = "^[^abc]$";
+		data = "d";
+		regExp(regExp, data);
+		data = "1";
+		regExp(regExp, data);
+		data = "ab";
+		regExp(regExp, data);
+	}
+}
