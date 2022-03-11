2489f19d2e08927ad9cc3622e59a5a9eef806e59
Hyeongyu Lee <dlgusrb1913@naver.com>
2022-03-11 20:30:41 +0900

    Update Baekjoon_2580_sudoku.java

--- algorithm/src/BackTracking/Baekjoon_2580_sudoku.java
+++ algorithm/src/BackTracking/Baekjoon_2580_sudoku.java
@@ -21,9 +21,41 @@ public class Baekjoon_2580_sudoku {
 		}
 		
 		sudoku(0, 0);
+		System.out.println(sb);
 	}
 	
 	public static void sudoku(int row, int col) {
+		if(col == 9) {
+			sudoku(row + 1, 0);
+			return;
+		}
 		
+		if(row == 9) {
+			for(int i = 0; i < 9; i++) {
+				for(int j = 0; j < 9; j++) {
+					sb.append(sudoku[i][j]).append(' ');
+				}
+				sb.append('\n');
+			}
+			return;
+		}
+		
+		if(sudoku[row][col] == 0) {
+			for(int i = 1; i <= 9; i++) {
+				if(check(row, col, i)) {
+					sudoku[row][col] = i;
+					sudoku(row, col + 1);
+				}
+			}
+			sudoku[row][col] = 0;
+			return;
+		}
+		
+		sudoku(row, col + 1);
+	}
+	
+	public static boolean check(int row, int col, int val) {
+		
+		return true;
 	}
 }
